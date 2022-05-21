413108d4595ed97f7c4f016d9c5627724ebbbef4
Adwiti <adwitibishwanath@gmail.com>
2022-05-21 12:43:07 +0530

    Implemented Sign Out on MainActivity.kt

--- app/src/main/java/com/example/sociopigeon/MainActivity.kt
+++ app/src/main/java/com/example/sociopigeon/MainActivity.kt
@@ -5,20 +5,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.example.sociopigeon.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mainBinding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
 
-        mainBinding.fab.setOnClickListener(){
+        mainBinding.fab.setOnClickListener {
 
             val intent=Intent(this,CreatePostActivity::class.java)
             startActivity(intent)
 
         }
 
+        auth = Firebase.auth
+        mainBinding.signOut.setOnClickListener{
+            val intent = Intent(this, SignInActivity::class.java)
+            auth.signOut()
+            startActivity(intent)
+            finish()
+        }
+
     }
+
 }
