aa8d9e18941448aec0f67cf78054f3e5a1a03ada
Adwiti <adwitibishwanath@gmail.com>
2022-05-28 14:25:04 +0530

    Implemented Add Post Activity and Recycler view for Posts.

--- app/src/main/java/com/example/sociopigeon/MainActivity.kt
+++ app/src/main/java/com/example/sociopigeon/MainActivity.kt
@@ -3,15 +3,24 @@ package com.example.sociopigeon
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
 import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.sociopigeon.daos.PostDao
 import com.example.sociopigeon.databinding.ActivityMainBinding
+import com.example.sociopigeon.models.Post
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.Query
 import com.google.firebase.ktx.Firebase
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), PostAdapter.IPostAdapter {
+    private lateinit var postDao: PostDao
+    private lateinit var adapter: PostAdapter
     private lateinit var mainBinding: ActivityMainBinding
     private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
@@ -19,19 +28,47 @@ class MainActivity : AppCompatActivity() {
 
         mainBinding.fab.setOnClickListener {
 
-            val intent=Intent(this,CreatePostActivity::class.java)
+            val intent = Intent(this, CreatePostActivity::class.java)
             startActivity(intent)
 
         }
 
         auth = Firebase.auth
-        mainBinding.signOut.setOnClickListener{
+        mainBinding.signOut.setOnClickListener {
             val intent = Intent(this, SignInActivity::class.java)
             auth.signOut()
             startActivity(intent)
             finish()
         }
+        setUpRecyclerView()
 
+    }
+
+    private fun setUpRecyclerView() {
+        postDao = PostDao()
+        val postCollections = postDao.postCollections
+        val query = postCollections.orderBy("Created at", Query.Direction.DESCENDING)
+        val recyclerViewOptions =
+            FirestoreRecyclerOptions.Builder<Post>().setQuery(query, Post::class.java).build()
+
+
+        adapter = PostAdapter(recyclerViewOptions, this)
+        mainBinding.recyclerView.adapter = adapter
+        mainBinding.recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        adapter.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        adapter.stopListening()
+    }
+
+    override fun onLikeClicked(postId: String) {
+        postDao.updateLikes(postId)
     }
 
 }
